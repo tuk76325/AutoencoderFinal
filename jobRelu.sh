1127d80d109a4db387c712131156730be1740a96
tuk76325 <93604400+tuk76325@users.noreply.github.com>
2023-12-10 05:00:45 -0500

    Add files via upload

---
+++ jobRelu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#PBS -l walltime=1:00:00
+#PBS -N mytestjob
+#PBS -q large
+
+module load cuda
+
+export PATH=/home/tuk76325/anaconda3/bin:$PATH
+
+source activate pytorch
+cd /home/tuk76325/work/PythonProjects/MinaAutoEncoder
+python main.py 160,260,300 relu
